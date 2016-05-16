2c2aee1f7993be7a3946811f7fb6fbbeb8eadb65
IanH <ian_web1@hubbertz.de>
2016-05-16 18:58:36 +0200

    Moved BMP180 sensor to new Class SensorNode

---
+++ src/SensorNode.cpp
@@ -0,0 +1,37 @@
+/*
+ * SensorNode.cpp
+ *
+ *  Created on: 16.05.2016
+ *      Author: ian
+ */
+
+#include "SensorNode.h"
+#include <Homie.hpp>
+
+SensorNode::SensorNode() :
+	HomieNode("Sensor", "sensor_t_p",
+		[](String property, String value) { return false; },
+		true),
+	lastLoop8000ms(0) {
+}
+
+void SensorNode::setup() {
+	Wire.begin();
+	Sensors::initialize();
+	char printbuf[50];
+	snprintf(printbuf, sizeof(printbuf),
+			"Initialized sensors at %x (Thermo), %x (Baro)",
+			Sensors::getThermometer(), Sensors::getBarometer());
+	Serial.println(printbuf);
+}
+
+void SensorNode::loop() {
+	if (millis() - lastLoop8000ms >= 8000UL || lastLoop8000ms == 0) {
+		float cur_temp = Sensors::getThermometer()->getTemperature();
+		float cur_press = Sensors::getBarometer()->getSealevelPressure(320.0);
+		lastLoop8000ms = millis();
+		Homie.setNodeProperty(*this, "degrees", String(cur_temp));
+		Homie.setNodeProperty(*this, "hPa", String(cur_press));
+	}
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <Homie.h>
 
-#include <Wire.h>
 
-#define SENSORS_BMP180_ATTACHED
-#include <Sensors.h>
-
-#include <Wire.h>
 #include "pcf8574.h"
 #include "Safety.h"
 #include "ValveNode.h"
 #include "RGBWNode.h"
+#include "SensorNode.h"
+
 #include "BewaesserungFSM.h"
 
 PCF8574 ioext(0x20);
@@ -21,12 +18,9 @@ bool pumpe_state(false);
 HomieNode automode("Auto", "switch");
 bool automatik = true;
 
-HomieNode temperatur("Temperatur", "temp");
-HomieNode pressure("Luftdruck", "press");
-
 ValveNode valves(ioext);
 RGBWNode ledstrip;
-
+SensorNode sensor;
 
 unsigned long lastLoop2000ms = 0;
 
@@ -56,42 +50,24 @@ bool autoHandler(String value) {
 	return true;
 }
 
-void loopHandler() {
-	 if (millis() - lastLoop2000ms >= 8000UL || lastLoop2000ms == 0) {
-		 float cur_temp = Sensors::getThermometer()->getTemperature();
-		 float cur_press = Sensors::getBarometer()->getSealevelPressure(320.0);
-		 lastLoop2000ms = millis();
-		 Homie.setNodeProperty(temperatur, "degrees", String(cur_temp));
-		 Homie.setNodeProperty(temperatur, "hPa", String(cur_press));
-	 }
-}
-
 void setup() {
 //	safety.init();
 
 	Homie.disableResetTrigger();
 	Homie.registerNode(pumpe);
 	Homie.registerNode(automode);
-	Homie.registerNode(temperatur);
-	Homie.registerNode(pressure);
 
 	// HomieNode derived Nodes
 	Homie.registerNode(ledstrip);
 	Homie.registerNode(valves);
+	Homie.registerNode(sensor);
 
-	Homie.setLoopFunction(loopHandler);
 	Homie.setup();
 
 	pumpe.subscribe("on", pumpeHandler);
 	automode.subscribe("on", autoHandler);
 
-	Wire.begin();
-	Sensors::initialize();
-	char printbuf[50];
-	snprintf(printbuf,sizeof(printbuf), "Initialized sensors at %x (Thermo), %x (Baro)",
-			Sensors::getThermometer(), Sensors::getBarometer());
-	Serial.println(printbuf);
-
+	sensor.setup();
 	valves.setup();
 	ledstrip.setup();
 }
@@ -99,5 +75,6 @@ void setup() {
 void loop() {
 //	safety.loop();
 	Homie.loop();
+	sensor.loop();
 	//bewaesserung.cycle();
 }
